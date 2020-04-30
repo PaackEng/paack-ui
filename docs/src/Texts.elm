f6a7e498dbf46007069d591a08689ec8209af0f0
Pedro <pedro.laracampos@gmail.com>
2020-04-30 14:34:32 -0300

    WMS-134 | Add new Text component (#2)
    * Add new Text component
    * Add letterSpacing, add body samples, add global font
    * Add Text's note
    * Remove leftovers
    * Fix mistype in elm.json (exposed-modules missing comma)

---
+++ docs/src/Texts.elm
@@ -0,0 +1,66 @@
+module Texts exposing (stories)
+
+import Element
+import Element.Border as Border
+import Element.Font as Font
+import UI.Text as Text
+import UIExplorer exposing (storiesOf)
+
+
+stories =
+    storiesOf
+        "Texts"
+        [ ( "Texts"
+          , textsView
+          , { note = """
+```elm
+"Wherever You Will Go"
+    |> Text.heading1
+    |> Text.toEl
+
+\"\"\"
+So lately, been wondering
+Who will be there to take my place?
+\"\"\"
+    |> Text.body1
+    |> Text.toEl
+```
+
+Futures discussions includes:
+* How we'll add responsives attributes (font-size may variate with device size).
+* * Maybe `Text.toEl <device>`
+* is `Text.toEl` really necessary?
+""" }
+          )
+        ]
+
+
+styles =
+    [ ( Text.heading1, "Heading1" )
+    , ( Text.heading2, "Heading2" )
+    , ( Text.heading3, "Heading3" )
+    , ( Text.heading4, "Heading4" )
+    , ( Text.heading5, "Heading5" )
+    , ( Text.heading6, "Heading6" )
+    , ( Text.subtitle1, "Subtitle1" )
+    , ( Text.subtitle2, "Subtitle2" )
+    , ( Text.body1, "Body1: While working on the Tron: Legacy soundtrack in 2010, Daft Punk met with Casablancas in their studio through a mutual friend. The duo, who are fans of Casablancas' band The Strokes, presented him with an instrumental demo track intended for use on Daft Punk's next album. Casablancas responded favorably to the demo upon listening to it and subsequently agreed to provide accompanying vocals, forming the basis for what would become \"Instant Crush\"." )
+    , ( Text.body2, "Body2: \"I Will Follow You into the Dark\" is a song by indie rock band Death Cab for Cutie, the third single from their fifth album Plans, released on August 30, 2005. Written and performed by Ben Gibbard, it is an acoustic solo ballad, and was recorded in monaural with a single microphone and little editing." )
+    , ( Text.caption, "Caption: Still From \"Elephant Gun\" video, photo by: Kristianna Smith" )
+    , ( Text.overline, "OVERLINE" )
+    ]
+
+
+styleView ( component, label ) =
+    label
+        |> component
+        |> Text.toEl
+
+
+textsView content =
+    styles
+        |> List.map styleView
+        |> Element.column
+            [ Element.spacing 20
+            ]
+        |> Element.layout [ Font.family [ Font.typeface "Inter", Font.sansSerif ] ]
---
+++ src/UI/Text.elm
@@ -0,0 +1,187 @@
+module UI.Text exposing
+    ( Text
+    , TextSize
+    , body1
+    , body2
+    , caption
+    , heading1
+    , heading2
+    , heading3
+    , heading4
+    , heading5
+    , heading6
+    , overline
+    , subtitle1
+    , subtitle2
+    , toEl
+    )
+
+import Element exposing (Attribute, Element)
+import Element.Font as Font
+import List
+
+
+type alias Properties =
+    { content : String
+    , size : TextSize
+    }
+
+
+type Text
+    = Text Properties
+
+
+type TextSize
+    = SizeHeading1
+    | SizeHeading2
+    | SizeHeading3
+    | SizeHeading4
+    | SizeHeading5
+    | SizeHeading6
+    | SizeSubtitle1
+    | SizeSubtitle2
+    | SizeBody1
+    | SizeBody2
+    | SizeCaption
+    | SizeOverline
+
+
+heading1 : String -> Text
+heading1 content =
+    Text (Properties content SizeHeading1)
+
+
+heading2 : String -> Text
+heading2 content =
+    Text (Properties content SizeHeading2)
+
+
+heading3 : String -> Text
+heading3 content =
+    Text (Properties content SizeHeading3)
+
+
+heading4 : String -> Text
+heading4 content =
+    Text (Properties content SizeHeading4)
+
+
+heading5 : String -> Text
+heading5 content =
+    Text (Properties content SizeHeading5)
+
+
+heading6 : String -> Text
+heading6 content =
+    Text (Properties content SizeHeading6)
+
+
+subtitle1 : String -> Text
+subtitle1 content =
+    Text (Properties content SizeSubtitle1)
+
+
+subtitle2 : String -> Text
+subtitle2 content =
+    Text (Properties content SizeSubtitle2)
+
+
+body1 : String -> Text
+body1 content =
+    Text (Properties content SizeBody1)
+
+
+body2 : String -> Text
+body2 content =
+    Text (Properties content SizeBody2)
+
+
+caption : String -> Text
+caption content =
+    Text (Properties content SizeCaption)
+
+
+overline : String -> Text
+overline content =
+    Text (Properties content SizeOverline)
+
+
+toEl : Text -> Element msg
+toEl text =
+    case text of
+        Text { content, size } ->
+            content
+                |> Element.text
+                |> List.singleton
+                |> Element.paragraph (deskAttributes size)
+
+
+
+-- Internal
+
+
+deskAttributes : TextSize -> List (Attribute msg)
+deskAttributes size =
+    case size of
+        SizeHeading1 ->
+            [ Font.size 92
+            , Font.letterSpacing -4.5
+            ]
+
+        SizeHeading2 ->
+            [ Font.size 64
+            , Font.letterSpacing -3
+            ]
+
+        SizeHeading3 ->
+            [ Font.size 48
+            , Font.letterSpacing -2
+            ]
+
+        SizeHeading4 ->
+            [ Font.size 32
+            , Font.letterSpacing -1
+            ]
+
+        SizeHeading5 ->
+            [ Font.size 24
+            , Font.letterSpacing 0
+            ]
+
+        SizeHeading6 ->
+            [ Font.size 20
+            , Font.letterSpacing 0.15
+            ]
+
+        SizeSubtitle1 ->
+            [ Font.size 16
+            , Font.letterSpacing 0.2
+            ]
+
+        SizeSubtitle2 ->
+            [ Font.size 14
+            , Font.letterSpacing 0.25
+            ]
+
+        SizeBody1 ->
+            [ Font.size 16
+            , Element.spacing 8
+            , Font.letterSpacing 0.2
+            ]
+
+        SizeBody2 ->
+            [ Font.size 14
+            , Element.spacing 10
+            , Font.letterSpacing 0.25
+            ]
+
+        SizeCaption ->
+            [ Font.size 12
+            , Element.spacing 4
+            , Font.letterSpacing 0.5
+            ]
+
+        SizeOverline ->
+            [ Font.size 10
+            , Font.letterSpacing 2
+            ]
