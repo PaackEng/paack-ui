989ecbc199af6ba37cedc0e960becac9ef824c6e
PedroHLC <root@pedrohlc.com>
2020-05-06 11:55:48 -0300

    Allow components to create more flexible Texts

---
+++ src/UI/Internal/Text.elm
@@ -0,0 +1,228 @@
+module UI.Internal.Text exposing (..)
+
+import Element exposing (Attribute, Element)
+import Element.Font as Font
+import List
+import UI.Internal.Palette as Palette
+import UI.Palette as Palette
+import UI.RenderConfig exposing (RenderConfig, isMobile)
+
+
+type alias Options =
+    { color : TextColor }
+
+
+type alias Properties =
+    { content : String
+    , size : TextSize
+    }
+
+
+type Text
+    = Text Properties Options
+
+
+type TextSize
+    = SizeHeading1
+    | SizeHeading2
+    | SizeHeading3
+    | SizeHeading4
+    | SizeHeading5
+    | SizeHeading6
+    | SizeSubtitle1
+    | SizeSubtitle2
+    | SizeBody1
+    | SizeBody2
+    | SizeCaption
+    | SizeOverline
+
+
+type TextColor
+    = ColorPalette Palette.Color
+    | ColorBgMiddle
+    | ColorBgLightest
+    | ColorBgLighter
+    | ColorBgDisabled
+    | ColorInherit
+
+
+defaultText : TextSize -> String -> Text
+defaultText size content =
+    ( Palette.toneGray, Palette.lumDarkest )
+        |> ColorPalette
+        |> Options
+        |> Text (Properties content size)
+
+
+fontColor : TextColor -> Maybe Element.Color
+fontColor color =
+    case color of
+        ColorPalette paletteColor ->
+            Just <| Palette.toElColor paletteColor
+
+        ColorBgMiddle ->
+            Just <| Palette.textBgMiddle
+
+        ColorBgLightest ->
+            Just <| Palette.textBgLightest
+
+        ColorBgLighter ->
+            Just <| Palette.textBgLighter
+
+        ColorBgDisabled ->
+            Just <| Palette.textComponentDisabled
+
+        ColorInherit ->
+            Nothing
+
+
+attributes : RenderConfig -> TextSize -> TextColor -> List (Attribute msg)
+attributes config size color =
+    let
+        sizeAttrs =
+            if isMobile config then
+                mobileAttributes size
+
+            else
+                deskAttributes size
+    in
+    case fontColor color of
+        Just attr ->
+            Font.color attr :: sizeAttrs
+
+        Nothing ->
+            sizeAttrs
+
+
+deskAttributes : TextSize -> List (Attribute msg)
+deskAttributes size =
+    case size of
+        SizeHeading1 ->
+            [ Font.size 92
+            , Font.letterSpacing -4.5
+            ]
+
+        SizeHeading2 ->
+            [ Font.size 64
+            , Font.letterSpacing -3
+            ]
+
+        SizeHeading3 ->
+            [ Font.size 48
+            , Font.letterSpacing -2
+            ]
+
+        SizeHeading4 ->
+            [ Font.size 32
+            , Font.letterSpacing -1
+            ]
+
+        SizeHeading5 ->
+            [ Font.size 24
+            , Font.letterSpacing 0
+            ]
+
+        SizeHeading6 ->
+            [ Font.size 20
+            , Font.letterSpacing 0.15
+            ]
+
+        SizeSubtitle1 ->
+            [ Font.size 16
+            , Font.letterSpacing 0.2
+            ]
+
+        SizeSubtitle2 ->
+            [ Font.size 14
+            , Font.letterSpacing 0.25
+            ]
+
+        SizeBody1 ->
+            [ Font.size 16
+            , Element.spacing 8
+            , Font.letterSpacing 0.2
+            ]
+
+        SizeBody2 ->
+            [ Font.size 14
+            , Element.spacing 10
+            , Font.letterSpacing 0.25
+            ]
+
+        SizeCaption ->
+            [ Font.size 12
+            , Element.spacing 4
+            , Font.letterSpacing 0.5
+            ]
+
+        SizeOverline ->
+            [ Font.size 10
+            , Font.letterSpacing 2
+            ]
+
+
+mobileAttributes : TextSize -> List (Attribute msg)
+mobileAttributes size =
+    case size of
+        SizeHeading1 ->
+            [ Font.size 56
+            , Font.letterSpacing -2
+            ]
+
+        SizeHeading2 ->
+            [ Font.size 48
+            , Font.letterSpacing -1.5
+            ]
+
+        SizeHeading3 ->
+            [ Font.size 40
+            , Font.letterSpacing -1
+            ]
+
+        SizeHeading4 ->
+            [ Font.size 32
+            , Font.letterSpacing -0.5
+            ]
+
+        SizeHeading5 ->
+            [ Font.size 24
+            , Font.letterSpacing 0
+            ]
+
+        SizeHeading6 ->
+            [ Font.size 20
+            , Font.letterSpacing 0.15
+            ]
+
+        SizeSubtitle1 ->
+            [ Font.size 16
+            , Font.letterSpacing 0.2
+            ]
+
+        SizeSubtitle2 ->
+            [ Font.size 14
+            , Font.letterSpacing 0.25
+            ]
+
+        SizeBody1 ->
+            [ Font.size 16
+            , Element.spacing 8
+            , Font.letterSpacing 0.2
+            ]
+
+        SizeBody2 ->
+            [ Font.size 14
+            , Element.spacing 10
+            , Font.letterSpacing 0.25
+            ]
+
+        SizeCaption ->
+            [ Font.size 12
+            , Element.spacing 4
+            , Font.letterSpacing 0.5
+            ]
+
+        SizeOverline ->
+            [ Font.size 10
+            , Font.letterSpacing 2
+            ]
--- src/UI/Text.elm
+++ src/UI/Text.elm
@@ -1,65 +1,37 @@
 module UI.Text exposing
     ( Text
-    , TextBrightness
+    , TextColor
     , body1
     , body2
     , caption
+    , colorPrimary
     , heading1
     , heading2
     , heading3
     , heading4
     , heading5
     , heading6
-    , lumBright
-    , lumBrightest
-    , lumNormal
     , overline
     , subtitle1
     , subtitle2
     , toEl
-    , withBrightness
+    , withColor
     )
 
 import Element exposing (Attribute, Element)
 import Element.Font as Font
 import List
-import UI.Internal.Palette as Palette
+import UI.Internal.Text as Internal exposing (..)
+import UI.Palette as Palette
 import UI.RenderConfig exposing (RenderConfig, isMobile)
 
 
-type alias Options =
-    { brightness : TextBrightness }
+type alias Text =
+    Internal.Text
 
 
-type alias Properties =
-    { content : String
-    , size : TextSize
-    }
-
-
-type Text
-    = Text Properties Options
-
-
-type TextSize
-    = SizeHeading1
-    | SizeHeading2
-    | SizeHeading3
-    | SizeHeading4
-    | SizeHeading5
-    | SizeHeading6
-    | SizeSubtitle1
-    | SizeSubtitle2
-    | SizeBody1
-    | SizeBody2
-    | SizeCaption
-    | SizeOverline
-
-
-type TextBrightness
-    = LumNormal
-    | LumBright
-    | LumBrightest
+type alias TextColor =
+    Internal.TextColor
 
 
 heading1 : String -> Text
@@ -122,201 +94,21 @@ overline content =
     defaultText SizeOverline content
 
 
-lumBright : TextBrightness
-lumBright =
-    LumBright
+colorPrimary : TextColor
+colorPrimary =
+    ColorPalette ( Palette.tonePrimary, Palette.lumMiddle )
 
 
-lumBrightest : TextBrightness
-lumBrightest =
-    LumBrightest
-
-
-lumNormal : TextBrightness
-lumNormal =
-    LumNormal
-
-
-withBrightness : TextBrightness -> Text -> Text
-withBrightness brightness (Text prop opt) =
-    Text prop { opt | brightness = brightness }
+withColor : TextColor -> Text -> Text
+withColor color (Text prop _) =
+    Text prop (Options color)
 
 
 toEl : RenderConfig -> Text -> Element msg
 toEl cfg text =
     case text of
-        Text { content, size } { brightness } ->
+        Text { content, size } { color } ->
             content
                 |> Element.text
                 |> List.singleton
-                |> Element.paragraph (attributes cfg size brightness)
-
-
-
--- Internal
-
-
-defaultText : TextSize -> String -> Text
-defaultText size content =
-    Text
-        (Properties content size)
-        (Options LumNormal)
-
-
-attributes : RenderConfig -> TextSize -> TextBrightness -> List (Attribute msg)
-attributes config size brightness =
-    let
-        fontColor =
-            Font.color <|
-                case brightness of
-                    LumNormal ->
-                        Palette.gray.darkest
-
-                    LumBright ->
-                        Palette.gray.middle
-
-                    LumBrightest ->
-                        Palette.gray.light
-
-        sizeAttrs =
-            if isMobile config then
-                mobileAttributes size
-
-            else
-                deskAttributes size
-    in
-    fontColor :: sizeAttrs
-
-
-deskAttributes : TextSize -> List (Attribute msg)
-deskAttributes size =
-    case size of
-        SizeHeading1 ->
-            [ Font.size 92
-            , Font.letterSpacing -4.5
-            ]
-
-        SizeHeading2 ->
-            [ Font.size 64
-            , Font.letterSpacing -3
-            ]
-
-        SizeHeading3 ->
-            [ Font.size 48
-            , Font.letterSpacing -2
-            ]
-
-        SizeHeading4 ->
-            [ Font.size 32
-            , Font.letterSpacing -1
-            ]
-
-        SizeHeading5 ->
-            [ Font.size 24
-            , Font.letterSpacing 0
-            ]
-
-        SizeHeading6 ->
-            [ Font.size 20
-            , Font.letterSpacing 0.15
-            ]
-
-        SizeSubtitle1 ->
-            [ Font.size 16
-            , Font.letterSpacing 0.2
-            ]
-
-        SizeSubtitle2 ->
-            [ Font.size 14
-            , Font.letterSpacing 0.25
-            ]
-
-        SizeBody1 ->
-            [ Font.size 16
-            , Element.spacing 8
-            , Font.letterSpacing 0.2
-            ]
-
-        SizeBody2 ->
-            [ Font.size 14
-            , Element.spacing 10
-            , Font.letterSpacing 0.25
-            ]
-
-        SizeCaption ->
-            [ Font.size 12
-            , Element.spacing 4
-            , Font.letterSpacing 0.5
-            ]
-
-        SizeOverline ->
-            [ Font.size 10
-            , Font.letterSpacing 2
-            ]
-
-
-mobileAttributes : TextSize -> List (Attribute msg)
-mobileAttributes size =
-    case size of
-        SizeHeading1 ->
-            [ Font.size 56
-            , Font.letterSpacing -2
-            ]
-
-        SizeHeading2 ->
-            [ Font.size 48
-            , Font.letterSpacing -1.5
-            ]
-
-        SizeHeading3 ->
-            [ Font.size 40
-            , Font.letterSpacing -1
-            ]
-
-        SizeHeading4 ->
-            [ Font.size 32
-            , Font.letterSpacing -0.5
-            ]
-
-        SizeHeading5 ->
-            [ Font.size 24
-            , Font.letterSpacing 0
-            ]
-
-        SizeHeading6 ->
-            [ Font.size 20
-            , Font.letterSpacing 0.15
-            ]
-
-        SizeSubtitle1 ->
-            [ Font.size 16
-            , Font.letterSpacing 0.2
-            ]
-
-        SizeSubtitle2 ->
-            [ Font.size 14
-            , Font.letterSpacing 0.25
-            ]
-
-        SizeBody1 ->
-            [ Font.size 16
-            , Element.spacing 8
-            , Font.letterSpacing 0.2
-            ]
-
-        SizeBody2 ->
-            [ Font.size 14
-            , Element.spacing 10
-            , Font.letterSpacing 0.25
-            ]
-
-        SizeCaption ->
-            [ Font.size 12
-            , Element.spacing 4
-            , Font.letterSpacing 0.5
-            ]
-
-        SizeOverline ->
-            [ Font.size 10
-            , Font.letterSpacing 2
-            ]
+                |> Element.paragraph (attributes cfg size color)
