cfd7941da077846a263dbd0386210d83af83bd42
PedroHLC <root@pedrohlc.com>
2020-05-28 15:21:13 -0300

    [Cleanup] Remove unused components

--- docs/src/Model.elm
+++ docs/src/Model.elm
@@ -4,20 +4,17 @@ module Model exposing
     )
 
 import Buttons.Model as Buttons
-import Form.State as FormState
 import Paginators.Model as Paginators
 
 
 type alias Model =
-    { formStories : FormState.Model
-    , buttonsStories : Buttons.Model
+    { buttonsStories : Buttons.Model
     , paginatorsStories : Paginators.Model
     }
 
 
 init : Model
 init =
-    { formStories = FormState.initModel
-    , buttonsStories = Buttons.initModel
+    { buttonsStories = Buttons.initModel
     , paginatorsStories = Paginators.initModel
     }
