28de296c8e6a3bc15e9e30588a2a7f264fad6d5c
PedroHLC <root@pedrohlc.com>
2020-06-26 12:08:55 -0300

    Finish Size

--- src/UI/Size.elm
+++ src/UI/Size.elm
@@ -1,23 +1,52 @@
-module UI.Size exposing (extraSmall, large, medium, small)
+module UI.Size exposing
+    ( extraSmall, small, medium, large
+    , Size
+    )
 
-import UI.Internal.Size as Internal exposing (Size)
+{-| Contextual size for buttons and icons, as described in the design system.
 
 
+# Sizes
+
+@docs extraSmall, small, medium, large
+
+-}
+
+import UI.Internal.Size as Internal
+
+
+{-| Upholds a size value.
+-}
+type alias Size =
+    Internal.Size
+
+
+{-| For extra-small-sized components.
+-}
 extraSmall : Size
 extraSmall =
     Internal.ExtraSmall
 
 
+{-| For large-sized components.
+-}
 large : Size
 large =
     Internal.Large
 
 
+{-| For small-sized components.
+-}
 small : Size
 small =
     Internal.Small
 
 
+{-| For medium-sized components.
+
+**NOTE**: This is the default value.
+
+-}
 medium : Size
 medium =
     Internal.Medium
