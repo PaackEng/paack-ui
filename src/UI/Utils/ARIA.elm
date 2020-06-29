7b0f2053744ae89bcf0bb8582c4b9c01d40c0d2e
PedroHLC <root@pedrohlc.com>
2020-06-29 11:28:03 -0300

    LAST LAP

--- src/UI/Utils/ARIA.elm
+++ src/UI/Utils/ARIA.elm
@@ -1,24 +1,65 @@
-module UI.Utils.ARIA exposing (Role, labelAttr, roleAttr, roleButton, roleImage)
+module UI.Utils.ARIA exposing
+    ( Role, roleButton, roleImage, roleAttr
+    , labelAttr
+    )
+
+{-| Interface for [HTML's ARIA](https://developer.mozilla.org/en-US/docs/Web/Accessibility/ARIA).
+
+
+# Role
+
+@docs Role, roleButton, roleImage, roleAttr
+
+
+# Label
+
+@docs labelAttr
+
+-}
 
 import Element exposing (Attribute)
 import Html.Attributes as HtmlAttrs
 
 
+{-| Roles defines the type of UI element.
+
+See [MDN article](https://developer.mozilla.org/en-US/docs/Web/Accessibility/ARIA/ARIA_Techniques#Roles).
+
+-}
 type Role
     = RoleButton
     | RoleImage
 
 
+{-| "The button role should be used for clickable elements that trigger a response when activated by the user." - MDN
+
+    Element.el
+        [ ARIA.roleAttr ARIA.roleButton
+        , Events.onClick Msg.SomeEvent
+        ]
+        someChildElement
+
+-}
 roleButton : Role
 roleButton =
     RoleButton
 
 
+{-| "Can be used to identify multiple elements inside page content that should be considered as a single image." - MDN
+
+    Element.el
+        [ ARIA.roleAttr ARIA.roleImage
+        ]
+        [ Element.text "😺 Meow" ]
+
+-}
 roleImage : Role
 roleImage =
     RoleImage
 
 
+{-| Transform a [`Role`](#Role) in a valid [`Element.Attribute`](/packages/mdgriffith/elm-ui/latest/Element#Attribute)
+-}
 roleAttr : Role -> Attribute msg
 roleAttr role =
     Element.htmlAttribute <|
