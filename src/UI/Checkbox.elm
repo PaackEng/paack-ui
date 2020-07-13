8e4b43ca75749a410ade9227fff6fc715ed1d40d
PedroHLC <root@pedrohlc.com>
2020-07-13 15:50:27 -0300

    Oops

--- src/UI/Checkbox.elm
+++ src/UI/Checkbox.elm
@@ -12,9 +12,17 @@ We have plans for "checklists", but this won't be created until usecase appears.
 
 -}
 
-import Element exposing (Element)
+import Element exposing (Attribute, Element, px)
+import Element.Background as Background
+import Element.Border as Border
+import Element.Events as Events
+import UI.Icon as Icon
+import UI.Internal.Palette as Palette
+import UI.Palette as Palette
 import UI.RenderConfig exposing (RenderConfig)
 import UI.Text as Text
+import UI.Utils.ARIA as ARIA
+import UI.Utils.Element as Element
 
 
 checkbox : RenderConfig -> (Bool -> msg) -> String -> Bool -> Element msg
@@ -26,7 +34,7 @@ checkbox renderConfig message label state =
             , Border.color Palette.primary.middle
             , Border.width 3
             , Border.rounded 8
-            , Events.onClick message
+            , Events.onClick (message (not state))
             , Element.pointer
             ]
                 ++ (ARIA.toElementAttributes <| ARIA.roleButton)
