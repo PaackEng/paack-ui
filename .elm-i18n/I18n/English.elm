15fd0ff843b9f47fb52400260260f3d0ae0676da
PedroHLC <root@pedrohlc.com>
2020-07-27 10:13:51 -0300

    We'll have to commit those files, otherwise it won't publish

---
+++ .elm-i18n/I18n/English.elm
@@ -0,0 +1,26 @@
+module I18n.English exposing (..)
+
+
+import I18n.Type exposing (..)
+
+
+filtersRange : FiltersRange
+filtersRange =
+    { after = "After"
+    , before = "Before"
+    , on = "On"
+    }
+
+
+filters : Filters
+filters =
+    { range = filtersRange
+    }
+
+
+root : Root
+root =
+    { filters = filters
+    }
+
+
---
+++ .elm-i18n/I18n/Type.elm
@@ -0,0 +1,20 @@
+module I18n.Type exposing (..)
+
+
+type alias FiltersRange =
+    { after : String
+    , before : String
+    , on : String
+    }
+
+
+type alias Filters =
+    { range : FiltersRange
+    }
+
+
+type alias Root =
+    { filters : Filters
+    }
+
+
