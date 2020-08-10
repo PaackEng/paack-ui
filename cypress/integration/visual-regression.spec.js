f30679045293fc06e8990d6f8b9471cc9a095f1d
Renan Vaz <renan.piresvz@gmail.com>
2020-08-10 10:08:08 -0300

    POC | Create a snapshot for each page

--- cypress/integration/visual-regression.spec.js
+++ cypress/integration/visual-regression.spec.js
@@ -1,11 +1,72 @@
-describe("Integration test with visual testing", () => {
-  it("Colors Page Snapshot", () => {
-    cy.visit("/#Styles/Colors/Colors");
-    cy.percySnapshot("colors");
-  });
+describe('Percy Snapshots', () => {
+  describe('Styles', () => {
+    it('Creates the Colors page snapshot', () => {
+      cy.visit('/#Styles/Colors/Colors')
+      cy.percySnapshot('colors')
+    })
 
-  it("Sizes Page Snapshot", () => {
-    cy.visit("/#Styles/Sizes/Large");
-    cy.percySnapshot("sizes");
-  });
-});
+    it('Creates the Sizes page snapshot', () => {
+      cy.visit('/#Styles/Sizes/Large')
+      cy.percySnapshot('sizes')
+    })
+  })
+
+  describe('Basics', () => {
+    it('Creates the Texts page snapshot', () => {
+      cy.visit('/#Basics/Texts/Texts')
+      cy.percySnapshot('texts')
+    })
+
+    it('Creates the Icons page snapshot', () => {
+      cy.visit('/#Basics/Icons/IconsExample')
+      cy.percySnapshot('icons')
+    })
+
+    it('Creates the Buttons page snapshot', () => {
+      cy.visit('/#Basics/Buttons/Primary')
+      cy.percySnapshot('buttons')
+    })
+
+    it('Creates the Alerts page snapshot', () => {
+      cy.visit('/#Basics/Alerts/Primary')
+      cy.percySnapshot('alerts')
+    })
+
+    it('Creates the Badges page snapshot', () => {
+      cy.visit('/#Basics/Badges/Badge%20gray')
+      cy.percySnapshot('badges')
+    })
+
+    it('Creates the TextField page snapshot', () => {
+      cy.visit('/#Basics/TextField/Default')
+      cy.percySnapshot('textfield')
+    })
+
+    it('Creates the Loading page snapshot', () => {
+      cy.visit('/#Basics/Loading/Small')
+      cy.percySnapshot('Loading')
+    })
+
+    it('Creates the Checkboxes page snapshot', () => {
+      cy.visit('/#Basics/Checkboxes/Checkboxes')
+      cy.percySnapshot('Checkboxes')
+    })
+
+    it('Creates the Radio page snapshot', () => {
+      cy.visit('/#Basics/Radio/Radio')
+      cy.percySnapshot('Radio')
+    })
+  })
+
+  describe('Complex components', () => {
+    it('Creates the Tables page snapshot', () => {
+      cy.visit('/#Complex%20components/Tables/Desktop')
+      cy.percySnapshot('Paginators')
+    })
+
+    it('Creates the Colors page snapshot', () => {
+      cy.visit('/#Complex%20components/Paginators/NonNumeric')
+      cy.percySnapshot('colors')
+    })
+  })
+})
