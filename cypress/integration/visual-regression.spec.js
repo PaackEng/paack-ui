579ef8b857d6e6f398fc6253d5f68e59018d6e9d
Renan Vaz <renan.piresvz@gmail.com>
2020-08-11 09:58:50 -0300

    Fix snapshot names

--- cypress/integration/visual-regression.spec.js
+++ cypress/integration/visual-regression.spec.js
@@ -2,44 +2,44 @@ describe('Percy Snapshots', () => {
   describe('Styles', () => {
     it('Creates the Colors page snapshot', () => {
       cy.visit('/#Styles/Colors/Colors')
-      cy.percySnapshot('colors')
+      cy.percySnapshot('Colors')
     })
 
     it('Creates the Sizes page snapshot', () => {
       cy.visit('/#Styles/Sizes/Large')
-      cy.percySnapshot('sizes')
+      cy.percySnapshot('Sizes')
     })
   })
 
   describe('Basics', () => {
     it('Creates the Texts page snapshot', () => {
       cy.visit('/#Basics/Texts/Texts')
-      cy.percySnapshot('texts')
+      cy.percySnapshot('Texts')
     })
 
     it('Creates the Icons page snapshot', () => {
       cy.visit('/#Basics/Icons/IconsExample')
-      cy.percySnapshot('icons')
+      cy.percySnapshot('Icons')
     })
 
     it('Creates the Buttons page snapshot', () => {
       cy.visit('/#Basics/Buttons/Primary')
-      cy.percySnapshot('buttons')
+      cy.percySnapshot('Buttons')
     })
 
     it('Creates the Alerts page snapshot', () => {
       cy.visit('/#Basics/Alerts/Primary')
-      cy.percySnapshot('alerts')
+      cy.percySnapshot('Alerts')
     })
 
     it('Creates the Badges page snapshot', () => {
       cy.visit('/#Basics/Badges/Badge%20gray')
-      cy.percySnapshot('badges')
+      cy.percySnapshot('Badges')
     })
 
     it('Creates the TextField page snapshot', () => {
       cy.visit('/#Basics/TextField/Default')
-      cy.percySnapshot('textfield')
+      cy.percySnapshot('TextField')
     })
 
     it('Creates the Loading page snapshot', () => {
@@ -61,12 +61,12 @@ describe('Percy Snapshots', () => {
   describe('Complex components', () => {
     it('Creates the Tables page snapshot', () => {
       cy.visit('/#Complex%20components/Tables/Desktop')
-      cy.percySnapshot('Paginators')
+      cy.percySnapshot('Tables')
     })
 
-    it('Creates the Colors page snapshot', () => {
+    it('Creates the Paginators page snapshot', () => {
       cy.visit('/#Complex%20components/Paginators/NonNumeric')
-      cy.percySnapshot('colors')
+      cy.percySnapshot('Paginators')
     })
   })
 })
