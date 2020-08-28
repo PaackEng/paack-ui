761117f2086f6fb0198e9257514a7ce95e6f0352
Renan Vaz <renan.piresvz@gmail.com>
2020-08-28 14:49:23 -0300

    Update ui assets

--- showcase/public/index.js
+++ showcase/public/index.js
@@ -1,5 +1,6 @@
 import { Elm } from './../src/Main.elm';
 import 'paack-ui-assets/js/paackSvgIconSprite';
+import 'paack-ui-assets/js/ellipsizableText';
 
 Elm.Main.init({
   node: document.getElementById('root'),
