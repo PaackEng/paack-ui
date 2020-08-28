cb0e55201695cef3bd8848b17944c869d35ce484
Jouderian <jouderianjr@gmail.com>
2020-08-28 14:16:42 +0200

    Remove navigators stories

--- showcase/src/Model.elm
+++ showcase/src/Model.elm
@@ -6,11 +6,9 @@ module Model exposing
 import Buttons.Model as Buttons
 import Checkboxes.Model as Checkboxes
 import Paginators.Model as Paginators
-import Navigators.Model as Navigators
 import Radio.Model as Radio
 import Tables.Model as Tables
 import Tabs.Model as Tabs
-import UI.RenderConfig as RenderConfig
 import UIExplorer.Plugins.Tabs as TabsPlugin
 
 
@@ -21,27 +19,17 @@ type alias Model =
     , checkboxesStories : Checkboxes.Model
     , radioStories : Radio.Model
     , tabsStories : Tabs.Model
-    , navigatorsStories : Navigators.Model 
     , tabs : TabsPlugin.Model
     }
 
 
 init : Model
 init =
-    let
-        renderConfig =
-            RenderConfig.init
-                { width = 1920
-                , height = 1080
-                }
-                RenderConfig.localeEnglish
-    in
     { buttonsStories = Buttons.initModel
     , paginatorsStories = Paginators.initModel
     , tablesStories = Tables.initModel
     , checkboxesStories = Checkboxes.initModel
     , radioStories = Radio.initModel
     , tabsStories = Tabs.initModel
-    , navigatorsStories = Navigators.initModel renderConfig 
     , tabs = TabsPlugin.initialModel
     }
--- showcase/src/Navigators/Msg.elm
+++
@@ -1,7 +0,0 @@
-module Navigators.Msg exposing (Msg(..))
-
-import UI.NavigationContainer as Nav
-
-
-type Msg
-    = ForNav Nav.Msg
