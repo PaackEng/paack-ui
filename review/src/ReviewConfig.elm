7eeb67db2e5e460c035ee6f6ca5ff35a11e505a8
segmentationfaulter <muhammadsackib@gmail.com>
2020-12-18 18:04:06 +0500

    add most of the rules

--- review/src/ReviewConfig.elm
+++ review/src/ReviewConfig.elm
@@ -1,25 +1,41 @@
 module ReviewConfig exposing (config)
 
-{-| Do not rename the ReviewConfig module or the config function, because
-`elm-review` will look for these.
-
-To add packages that contain rules, add them to this review project using
-
-    `elm install author/packagename`
-
-when inside the directory containing this file.
-
--}
-
 import NoDebug.Log
 import NoDebug.TodoOrToString
 import NoBooleanCase
 import Review.Rule exposing (Rule)
+import NoRedundantConcat
+import NoRedundantCons
+import NoMissingTypeAnnotation
+import NoUnused.Variables
+import NoUnused.CustomTypeConstructors
+import NoUnused.Dependencies
+import NoUnused.Exports
+import NoUnused.Parameters
+import NoUnused.Patterns
+import NoUnused.Variables
 
+-- Add rule regarding redundant concat
+-- NoUnused.CustomTypeConstructors
+-- NoUnused.Exports
+-- Add ExposeAll
+
+-- Missing rules present in elm-analyse
+-- MapNothingToNothing
+-- FileLoadFailed (?)
+-- UnnecessaryParens (Not needed since elm-format handles it)
+-- UnnecessaryPortModule (Not needed since compiler reports it)
+-- DuplicateImport (Not needed since elm-format handles it)
 
 config : List Rule
 config =
     [ NoDebug.Log.rule
     , NoDebug.TodoOrToString.rule
     , NoBooleanCase.rule
+    , NoRedundantCons.rule
+    , NoMissingTypeAnnotation.rule
+    , NoUnused.Dependencies.rule
+    , NoUnused.Parameters.rule
+    , NoUnused.Patterns.rule
+    , NoUnused.Variables.rule
     ]
