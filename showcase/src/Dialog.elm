ea70173001237de5e49dbf2e183b0487617003e2
segmentationfaulter <muhammadsackib@gmail.com>
2020-12-30 15:28:04 +0500

    Add mobile story

--- showcase/src/Dialog.elm
+++ showcase/src/Dialog.elm
@@ -2,12 +2,12 @@ module Dialog exposing (stories)
 
 import Element
 import Msg
-import PluginOptions exposing (defaultWithoutMenu)
+import PluginOptions exposing (defaultWithMenu)
 import UI.Button as Button exposing (Button)
 import UI.Icon as Icon
 import UI.Internal.Colors exposing (overlayBackground)
 import UI.Internal.DialogV2 exposing (dialogViewV2)
-import UI.RenderConfig exposing (RenderConfig)
+import UI.RenderConfig as RenderConfig exposing (RenderConfig)
 import UI.Text as Text
 import UI.V2.Dialog as Dialog
 import UIExplorer exposing (storiesOf)
@@ -18,31 +18,77 @@ stories : RenderConfig -> ExplorerUI
 stories cfg =
     storiesOf
         "Dialog"
-        [ dialog cfg
+        [ dialogDesktop cfg
+        , dialogMobile
         ]
 
 
-dialog : RenderConfig -> ExplorerStory
-dialog cfg =
+dialogDesktop : RenderConfig -> ExplorerStory
+dialogDesktop cfg =
     story
-        ( "Dialog"
+        ( "Dialog Desktop"
         , Element.column
             [ Element.centerX
             , Element.centerY
             , Element.padding 20
             , Element.height <| Element.px 500
-            , overlayBackground
+            , Element.width Element.fill
             ]
             [ iconsSvgSprite
-            , Dialog.dialog "Dialog title"
-                (Icon.warning "Warning dialog")
+            , Dialog.dialog demoText.title
+                (Icon.warning demoText.icon)
                 |> Dialog.withBody
-                    ("Dialog body text" |> Text.body2 |> Text.renderElement cfg)
+                    (demoText.body |> Text.body2 |> Text.renderElement cfg)
                 |> Dialog.withButtons buttons
                 |> dialogViewV2 cfg
             ]
-        , { defaultWithoutMenu | code = code }
+        , { defaultWithMenu | code = code }
         )
+
+
+dialogMobile : ExplorerStory
+dialogMobile =
+    story
+        ( "Dialog Mobile"
+        , Element.column
+            [ Element.height <| Element.px mobileResolution.height
+            , Element.width <| Element.px mobileResolution.width
+            , Element.centerX
+            ]
+            [ iconsSvgSprite
+            , Dialog.dialog demoText.title
+                (Icon.warning demoText.icon)
+                |> Dialog.withBody
+                    (demoText.body
+                        |> Text.body2
+                        |> Text.renderElement
+                            mobileCfg
+                    )
+                |> Dialog.withButtons buttons
+                |> dialogViewV2 mobileCfg
+            ]
+        , { defaultWithMenu | code = code }
+        )
+
+
+demoText : { title : String, body : String, icon : String }
+demoText =
+    { title = "Dialog title"
+    , body = "There is no one who loves pain itself, who seeks after it and wants to have it, simply because it is pain..."
+    , icon = "Warning dialog"
+    }
+
+
+mobileResolution : { height : Int, width : Int }
+mobileResolution =
+    { width = 375, height = 667 }
+
+
+mobileCfg : RenderConfig
+mobileCfg =
+    RenderConfig.init
+        mobileResolution
+        RenderConfig.localeEnglish
 
 
 buttons : List (Button Msg.Msg)
