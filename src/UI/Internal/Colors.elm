881fe4fe4184fefef6cb978aba426d402c14742a
PedroHLC <root@pedrohlc.com>
2021-07-12 12:50:52 -0300

    The new internal colors

--- src/UI/Internal/Colors.elm
+++ src/UI/Internal/Colors.elm
@@ -1,154 +1,408 @@
-module UI.Internal.Colors exposing
-    ( ToneColors
-    , contrastDanger
-    , contrastPrimary
-    , contrastSuccess
-    , contrastWarning
-    , danger
-    , darkConstrast
-    , gray
-    , mainBackground
-    , overlayBackground
-    , primary
-    , success
-    , warning
-    , white
-    )
+module UI.Internal.Colors exposing (..)
 
 import Element exposing (Attribute, rgb255)
 import Element.Background as Background
 
 
 
--- Tones
+-- Generics
 
 
-type alias ToneColors =
-    { dark1 : Element.Color
-    , middle : Element.Color
-    , light1 : Element.Color
-    , light2 : Element.Color
-    , light3 : Element.Color
-    , light4 : Element.Color
-    }
-
-
-gray : ToneColors
-gray =
-    { dark1 = rgb255 14 20 32 -- #0E1420
-    , middle = rgb255 74 74 74 -- #4A4A4A
-    , light1 = rgb255 136 136 136 -- #888888
-    , light2 = rgb255 228 228 228 -- #E4E4E4
-    , light3 = rgb255 246 246 246 -- #F6F6F6
-    , light4 = rgb255 250 250 250 -- #FAFAFA
-    }
-
-
-primary : ToneColors
-primary =
-    { dark1 = rgb255 12 66 156 -- #0C429C
-    , middle = rgb255 27 96 216 -- #1B60D8
-    , light1 = rgb255 81 145 255 -- #5191FF
-    , light2 = rgb255 147 187 255 -- #93BBFF
-    , light3 = rgb255 227 238 255 -- #E3EEFF
-    , light4 = rgb255 244 248 255 -- #F4F8FF
-    }
-
-
-success : ToneColors
-success =
-    { dark1 = rgb255 1 167 76 -- #01A74C
-    , middle = rgb255 34 229 123 -- #22E57B
-    , light1 = rgb255 95 255 168 -- #5FFFA8
-    , light2 = rgb255 183 255 216 -- #B7FFD8
-    , light3 = rgb255 220 255 236 -- #DCFFEC
-    , light4 = rgb255 242 255 248 -- #F2FFF8
-    }
-
-
-danger : ToneColors
-danger =
-    { dark1 = rgb255 182 0 24 -- #B60018
-    , middle = rgb255 239 19 49 -- #EF1331
-    , light1 = rgb255 255 94 116 -- #FF5E74
-    , light2 = rgb255 255 174 185 -- #FFAEB9
-    , light3 = rgb255 255 232 235 -- #FFE8EB
-    , light4 = rgb255 255 245 246 -- #FFF5F6
-    }
-
-
-warning : ToneColors
-warning =
-    { dark1 = rgb255 148 133 0 -- #948500
-    , middle = rgb255 252 226 1 -- #FCE201
-    , light1 = rgb255 255 236 68 -- #FFEC44
-    , light2 = rgb255 255 245 157 -- #FFF59D
-    , light3 = rgb255 255 249 201 -- #FFF9C9
-    , light4 = rgb255 255 252 232 -- #FFFCE8
-    }
+black : Element.Color
+black =
+    rgb255 0 0 0
 
 
 white : Element.Color
 white =
-    rgb255 255 255 255
+    rgb255 0xFF 0xFF 0xFF
+
+
+skyBlue : Element.Color
+skyBlue =
+    rgb255 0x00 0xA3 0xE0
 
 
 
--- Contrasted Tones
+-- Shades baremetal
 
 
-darkConstrast : ToneColors
-darkConstrast =
-    { dark1 = white
-    , middle = white
-    , light1 = white
-    , light2 = gray.dark1
-    , light3 = gray.dark1
-    , light4 = gray.dark1
+gray800 : Element.Color
+gray800 =
+    rgb255 0x1D 0x20 0x21
+
+
+gray700 : Element.Color
+gray700 =
+    rgb255 0x3F 0x45 0x49
+
+
+gray600 : Element.Color
+gray600 =
+    rgb255 0x73 0x7C 0x82
+
+
+gray500 : Element.Color
+gray500 =
+    rgb255 0x9C 0xA5 0xAB
+
+
+gray400 : Element.Color
+gray400 =
+    rgb255 0xC2 0xC8 0xCB
+
+
+gray300 : Element.Color
+gray300 =
+    rgb255 0xDE 0xE1 0xE3
+
+
+gray200 : Element.Color
+gray200 =
+    rgb255 0xF1 0xF2 0xF3
+
+
+gray100 : Element.Color
+gray100 =
+    rgb255 0xF9 0xFA 0xFA
+
+
+navyBlue800 : Element.Color
+navyBlue800 =
+    rgb255 0x00 0x26 0x3E
+
+
+navyBlue700 : Element.Color
+navyBlue700 =
+    rgb255 0x19 0x4B 0x67
+
+
+navyBlue600 : Element.Color
+navyBlue600 =
+    rgb255 0x4C 0x7B 0x99
+
+
+navyBlue500 : Element.Color
+navyBlue500 =
+    rgb255 0x6F 0xA4 0xC3
+
+
+navyBlue400 : Element.Color
+navyBlue400 =
+    rgb255 0x91 0xBD 0xD9
+
+
+navyBlue300 : Element.Color
+navyBlue300 =
+    rgb255 0xBA 0xD6 0xE8
+
+
+navyBlue200 : Element.Color
+navyBlue200 =
+    rgb255 0xD6 0xE9 0xF5
+
+
+navyBlue100 : Element.Color
+navyBlue100 =
+    rgb255 0xEC 0xF3 0xF9
+
+
+green800 : Element.Color
+green800 =
+    rgb255 0x06 0x4E 0x2B
+
+
+green700 : Element.Color
+green700 =
+    rgb255 0x06 0x72 0x4B
+
+
+green600 : Element.Color
+green600 =
+    rgb255 0x07 0xAB 0x66
+
+
+green500 : Element.Color
+green500 =
+    rgb255 0x2A 0xCA 0x7E
+
+
+green400 : Element.Color
+green400 =
+    rgb255 0x6E 0xE7 0x9E
+
+
+green300 : Element.Color
+green300 =
+    rgb255 0xA7 0xF3 0xCA
+
+
+green200 : Element.Color
+green200 =
+    rgb255 0xD1 0xFA 0xE1
+
+
+green100 : Element.Color
+green100 =
+    rgb255 0xEC 0xFD 0xF5
+
+
+yellow800 : Element.Color
+yellow800 =
+    rgb255 0x7B 0x44 0x03
+
+
+yellow700 : Element.Color
+yellow700 =
+    rgb255 0xC2 0x6C 0x0A
+
+
+yellow600 : Element.Color
+yellow600 =
+    rgb255 0xEB 0xA0 0x0A
+
+
+yellow500 : Element.Color
+yellow500 =
+    rgb255 0xFB 0xC2 0x2D
+
+
+yellow400 : Element.Color
+yellow400 =
+    rgb255 0xFC 0xD3 0x4D
+
+
+yellow300 : Element.Color
+yellow300 =
+    rgb255 0xFD 0xE6 0x8A
+
+
+yellow200 : Element.Color
+yellow200 =
+    rgb255 0xFE 0xF3 0xC7
+
+
+yellow100 : Element.Color
+yellow100 =
+    rgb255 0xFF 0xFB 0xEB
+
+
+red800 : Element.Color
+red800 =
+    rgb255 0x8C 0x10 0x10
+
+
+red700 : Element.Color
+red700 =
+    rgb255 0xB9 0x25 0x1C
+
+
+red600 : Element.Color
+red600 =
+    rgb255 0xDE 0x37 0x2C
+
+
+red500 : Element.Color
+red500 =
+    rgb255 0xF6 0x5A 0x4F
+
+
+red400 : Element.Color
+red400 =
+    rgb255 0xFF 0x87 0x81
+
+
+red300 : Element.Color
+red300 =
+    rgb255 0xFB 0xBA 0xBA
+
+
+red200 : Element.Color
+red200 =
+    rgb255 0xFE 0xE2 0xE2
+
+
+red100 : Element.Color
+red100 =
+    rgb255 0xFE 0xF3 0xF2
+
+
+
+-- Shades sum composition
+
+
+type alias ColorPair =
+    { background : Element.Color
+    , text : Maybe Element.Color
     }
 
 
-lightContrast : ToneColors
-lightContrast =
-    { dark1 = white
-    , middle = gray.dark1
-    , light1 = gray.dark1
-    , light2 = gray.dark1
-    , light3 = gray.dark1
-    , light4 = gray.dark1
+type alias Shades =
+    { shade800 : ColorPair
+    , shade700 : ColorPair
+    , shade600 : ColorPair
+    , shade500 : ColorPair
+    , shade400 : ColorPair
+    , shade300 : ColorPair
+    , shade200 : ColorPair
+    , shade100 : ColorPair
     }
 
 
-contrastPrimary : ToneColors
-contrastPrimary =
-    darkConstrast
+gray : Shades
+gray =
+    { shade800 = ColorPair gray800 (Just white)
+    , shade700 = ColorPair gray700 (Just white)
+    , shade600 = ColorPair gray600 Nothing
+    , shade500 = ColorPair gray500 (Just navyBlue800)
+    , shade400 = ColorPair gray400 (Just navyBlue800)
+    , shade300 = ColorPair gray300 (Just navyBlue800)
+    , shade200 = ColorPair gray200 (Just navyBlue800)
+    , shade100 = ColorPair gray100 (Just navyBlue800)
+    }
 
 
-contrastSuccess : ToneColors
-contrastSuccess =
-    lightContrast
+navyBlue : Shades
+navyBlue =
+    { shade800 = ColorPair navyBlue800 (Just white)
+    , shade700 = ColorPair navyBlue700 (Just white)
+    , shade600 = ColorPair navyBlue600 Nothing
+    , shade500 = ColorPair navyBlue500 (Just navyBlue800)
+    , shade400 = ColorPair navyBlue400 (Just navyBlue800)
+    , shade300 = ColorPair navyBlue300 (Just navyBlue800)
+    , shade200 = ColorPair navyBlue200 (Just navyBlue800)
+    , shade100 = ColorPair navyBlue100 (Just navyBlue800)
+    }
 
 
-contrastDanger : ToneColors
-contrastDanger =
-    darkConstrast
+green : Shades
+green =
+    { shade800 = ColorPair green800 (Just white)
+    , shade700 = ColorPair green700 (Just white)
+    , shade600 = ColorPair green600 Nothing
+    , shade500 = ColorPair green500 (Just navyBlue800)
+    , shade400 = ColorPair green400 (Just navyBlue800)
+    , shade300 = ColorPair green300 (Just navyBlue800)
+    , shade200 = ColorPair green200 (Just navyBlue800)
+    , shade100 = ColorPair green100 (Just navyBlue800)
+    }
 
 
-contrastWarning : ToneColors
-contrastWarning =
-    lightContrast
+yellow : Shades
+yellow =
+    { shade800 = ColorPair yellow800 (Just white)
+    , shade700 = ColorPair yellow700 Nothing
+    , shade600 = ColorPair yellow600 Nothing
+    , shade500 = ColorPair yellow500 (Just navyBlue800)
+    , shade400 = ColorPair yellow400 (Just navyBlue800)
+    , shade300 = ColorPair yellow300 (Just navyBlue800)
+    , shade200 = ColorPair yellow200 (Just navyBlue800)
+    , shade100 = ColorPair yellow100 (Just navyBlue800)
+    }
+
+
+red : Shades
+red =
+    { shade800 = ColorPair red800 (Just white)
+    , shade700 = ColorPair red700 (Just white)
+    , shade600 = ColorPair red600 Nothing
+    , shade500 = ColorPair red500 (Just navyBlue800)
+    , shade400 = ColorPair red400 (Just navyBlue800)
+    , shade300 = ColorPair red300 (Just navyBlue800)
+    , shade200 = ColorPair red200 (Just navyBlue800)
+    , shade100 = ColorPair red100 (Just navyBlue800)
+    }
 
 
 
--- Text
--- Background
+-- Shades prod composition
 
 
-mainBackground : Attribute msg
-mainBackground =
-    Background.color white
+type Shade
+    = Shade800
+    | Shade700
+    | Shade600
+    | Shade500
+    | Shade400
+    | Shade300
+    | Shade200
+    | Shade100
 
 
-overlayBackground : Attribute msg
-overlayBackground =
-    Background.color (Element.rgba255 0 0 0 0.5)
+allShades : List Shade
+allShades =
+    [ Shade800, Shade700, Shade600, Shade500, Shade400, Shade300, Shade200, Shade100 ]
+
+
+type Tone
+    = Gray
+    | NavyBlue
+    | Green
+    | Yellow
+    | Red
+
+
+allTones : List Tone
+allTones =
+    [ Gray, NavyBlue, Green, Yellow, Red ]
+
+
+colorFromShades : Shade -> Shades -> ColorPair
+colorFromShades shade shades =
+    case shade of
+        Shade800 ->
+            shades.shade800
+
+        Shade700 ->
+            shades.shade700
+
+        Shade600 ->
+            shades.shade600
+
+        Shade500 ->
+            shades.shade500
+
+        Shade400 ->
+            shades.shade400
+
+        Shade300 ->
+            shades.shade300
+
+        Shade200 ->
+            shades.shade200
+
+        Shade100 ->
+            shades.shade100
+
+
+shadesFromTone : Tone -> Shades
+shadesFromTone tone =
+    case tone of
+        Gray ->
+            gray
+
+        NavyBlue ->
+            navyBlue
+
+        Green ->
+            green
+
+        Yellow ->
+            yellow
+
+        Red ->
+            red
+
+
+getBackgroundColor : Tone -> Shade -> Element.Color
+getBackgroundColor tone shade =
+    shadesFromTone tone
+        |> colorFromShades shade
+        |> .background
+
+
+getTextColor : Tone -> Shade -> Maybe Element.Color
+getTextColor tone shade =
+    shadesFromTone tone
+        |> colorFromShades shade
+        |> .text
