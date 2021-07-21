9dba29c63a34978ab2efba3e50e8cd55fa7bdd00
PedroHLC <root@pedrohlc.com>
2021-07-21 14:37:21 -0300

    WIP

---
+++ src/UI/Internal/Filter/Apply.elm
@@ -0,0 +1,23 @@
+module UI.Internal.Filter.Apply exposing (apply)
+
+import UI.Internal.Filter.Model as Filter exposing (Filter)
+import UI.Internal.Filter.Sorter as Sorter
+
+
+apply : Filter msg item -> Maybe (Sorter.Status item) -> List item -> List item
+apply filter sorting items =
+    let
+        filteredResults =
+            case Filter.filterGet filter of
+                Just filterFunction ->
+                    List.filter filterFunction items
+
+                Nothing ->
+                    items
+    in
+    case sorting of
+        Just ( Just direction, sorter ) ->
+            Sorter.sort sorter filteredResults
+
+        _ ->
+            filteredResults
