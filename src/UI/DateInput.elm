a589b60ebe0792af6238dcbfe2f34f4d1e5e2b7d
Greg Ziegan <greg.ziegan@gmail.com>
2021-09-29 09:42:58 -0500

    expose DateInput module

---
+++ src/UI/DateInput.elm
@@ -0,0 +1,42 @@
+module UI.DateInput exposing (DateInput, PeriodComparison, PeriodDate, RangeDate, fromPosix, toDD_MM_YYYY)
+
+import Time
+import UI.Internal.DateInput as Internal
+
+
+{-| `DateInput.DateInput` is used for describing dates in the UI.
+-}
+type alias DateInput =
+    Internal.DateInput
+
+
+{-| `DateInput.PeriodComparison` is used to compare date and time ranges.
+-}
+type alias PeriodComparison =
+    Internal.PeriodComparison
+
+
+{-| `DateInput.PeriodDate` is used to contain both date and comparison information.
+-}
+type alias PeriodDate =
+    Internal.PeriodDate
+
+
+{-| `DateInput.RangeDate` is used to describe a range of dates.
+-}
+type alias RangeDate =
+    Internal.RangeDate
+
+
+{-| Transforms a string into a `DateInput.DateInput`
+-}
+toDD_MM_YYYY : String -> DateInput -> String
+toDD_MM_YYYY =
+    Internal.toDD_MM_YYYY
+
+
+{-| Transform a time with time zone into a DateInput
+-}
+fromPosix : Time.Zone -> Time.Posix -> DateInput
+fromPosix =
+    Internal.fromPosix
